76dd21a4e871319b76535a43e149fe1929c2c102
rifam <vinicius.rifam@gmail.com>
2019-07-05 17:01:22 -0300

    alterações icones e footer
     foi implementado o footer em layout_2col, porém será futuramente implementado em uma pagina separada, os modelos do Atom não foram efetivos, os icones e links também serão implementados em paginas separadas

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -4,15 +4,12 @@
   <h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
 <?php end_slot() ?>
 
-<?php slot('sidebar') ?>
-
-  <?php echo get_component('menu', 'staticPagesMenu') ?>
-
-   <section>
-    <h2><?php echo __('Browse by') ?></h2>
+<?php slot('teste') ?>
+  <section>
+    <h2><?php echo __('Teste') ?></h2>
         <ul>
           <?php $icons = array(
-            'browseInformationObjects' => '/images/icons-large/icon-archival.png',
+            'browseInformationObjects' => '/images/icons-large/gear-teste.png',
             'browseActors' => '/images/icons-large/icon-people.png',
             'browseRepositories' => '/images/icons-large/icon-institutions.png',
             'browseSubjects' => '/images/icons-large/icon-subjects.png',
@@ -25,19 +22,41 @@
               <li>
                 <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
                   <?php if (isset($icons[$item->name])): ?>
-                    <?php echo image_tag($icons[$item->name], array('width' => 30, 'height' => 30, 'alt' => '')) ?>
+                    <?php echo image_tag($icons[$item->name], array('width' => 42, 'height' =>42, 'alt' => '')) ?>
                     <?php endif; ?>
                   <?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?>
                 </a>
               </li>
+              <br>
             <?php endforeach; ?>
           <?php endif; ?>
         </ul>
-    </section> 
+  </section> 
+<?php end_slot() ?>
+
+<?php slot('sidebar') ?>
+
+  <?php echo get_component('menu', 'staticPagesMenu') ?>
+
+  <section>
+    <h2><?php echo __('Browse by') ?></h2>
+    <ul>
+      <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
+      <?php if ($browseMenu->hasChildren()): ?>
+        <?php foreach ($browseMenu->getChildren() as $item): ?>
+          <li><a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>"><?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?></a></li>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </ul>
+  </section>
 
   <?php echo get_component('default', 'popular', array('limit' => 10, 'sf_cache_key' => $sf_user->getCulture())) ?>
 
 <?php end_slot() ?>
+<?php slot('footer') ?>
+<p> teste footer</p>
+<?php end_slot() ?>
+
 
 <div class="page">
   <?php echo render_value($sf_data->getRaw('content')) ?>
@@ -51,4 +70,6 @@
       </ul>
     </section>
   <?php end_slot() ?>
+
+
 <?php endif; ?>
--- templates/layout_2col.php
+++ templates/layout_2col.php
@@ -22,44 +22,82 @@
     
     <div id="wrapper" class="container" role="main">
 
-      <?php echo get_component('default', 'alerts') ?>  
-      <div id="homepage-hero">
-      </div>
+      <?php echo get_component('default', 'alerts') ?>
       <div class="row">
-        <div class="span3">
+       <div id="homepage-hero">
+      </div>
+      <!--- <div class="span3">
 
           <div id="sidebar">
-
-            <?php include_slot('sidebar') ?>
-
+            <?//php include_slot('sidebar') ?>
           </div>
-        </div>
-        <div class="span9">
-
+      </div>  --->
+      <div class="span9">
+          
           <div id="main-column">
+            <?//php include_slot('title') ?>  <!--- bem vindo apenas -->
+            <?php include_slot('teste') ?>
+           
+            <?//php include_slot('before-content') ?>
 
-            <?php include_slot('title') ?>
-
-            <?php include_slot('before-content') ?>
-
-            <?php if (!include_slot('content')): ?>
+            <?//php if (!include_slot('content')): ?>
+            <!--- 
               <div id="content">
-                <?php echo $sf_content ?>
-              </div>
-            <?php endif; ?>
+                <?//php echo $sf_content ?>
+              </div> -->
+            <?//php endif; ?>
 
-            <?php include_slot('after-content')?>
+            <?//php include_slot('after-content')?>
 
           </div>
 
         </div>
+      </div>
+    </div>
+
+    <?//php include_slot('post') ?>
+
+    <?//php echo get_partial('footer') ?>
+  <footer>
+  <div class="container">
+
+    <div class="row">
+
+      <div class="span3 offset1">
+        <h5>Relevant links</h5>
+        <ul>
+          <li><a href="http://www.accesstomemory.org/">www.accesstomemory.org</a></li>
+          <li><a href="https://www.accesstomemory.org/en/docs/2.3/">AtoM Documentation</a></li>
+          <li><a href="https://wiki.accesstomemory.org/Main_Page">AtoM Wiki</a></li>
+          <li><a href="https://www.accesstomemory.org/en/docs/2.3/admin-manual/customization/theming/#customization-theming">AtoM Theming Documentation</a></li>
+          <li><a href="https://groups.google.com/forum/#!forum/ica-atom-users">AtoM Users Forum</a></li>
+          <li><a href="https://www.artefactual.com/">Artefactual</a></li>
+        </ul>
+      </div>
+
+      <div class="span2">
+        <h5>Contact us</h5>
+        <ul>
+          <li><a href="mailto:demo@example.com">Email</a></li>
+          <li><a href="https://twitter.com/accesstomemory">Twitter</a></li>
+        </ul>
+      </div>
+
+      <div class="span3 offset3">
+        <h5>Powered by</h5>
+        <a href="http://www.accesstomemory.org"><?php echo image_tag('/plugins/arDemoThemePlugin/images/atom-logo.png', array('id' => 'atom-logo')) ?></a>
       </div>
 
     </div>
 
-    <?php include_slot('post') ?>
+    <?php if (QubitAcl::check('userInterface', 'translate')): ?>
+      <?php echo get_component('sfTranslatePlugin', 'translate') ?>
+    <?php endif; ?>
 
-    <?php echo get_partial('footer') ?>
+    <?php echo get_component_slot('footer') ?>
+
+  </div>
+  </footer>
 
   </body>
 </html>
